6db448f5cf0afd090d862865ba3352a25261210e
tomasz-brak <tomy.brak@gmail.com>
2026-06-02 12:34:04 +0200

    bubble sort

--- lab11/main.cpp
+++ lab11/main.cpp
@@ -1,10 +1,13 @@
+#include <memory>
 #include <print>
+#include <utility>
 #include <vector>
 
 template <typename T> using vec = std::vector<T>;
 
 class cTablica {
-  vec<int> dane = {};
+public:
+  vec<int> data = {32, 2, 13, 14, 17, 93, 1003, 3, 1004};
   void sortbobel();
   void sortwach();
   void sortlomuto();
@@ -12,7 +15,26 @@ class cTablica {
   void sortkopcowanie();
 };
 
+void cTablica::sortbobel() {
+  if (data.size() < 2) {
+    return;
+  }
+  bool isSorted = false;
+  while (!isSorted) {
+    isSorted = true;
+    for (auto it = data.begin(); it < data.end() - 1; it++) {
+      if (*it > *(it + 1)) {
+        isSorted = false;
+        std::swap(*it, *(it + 1));
+      }
+    }
+  }
+}
+
 int main(int argc, char *argv[]) {
   std::println("Hello world");
+  auto tab = cTablica();
+  tab.sortbobel();
+  std::println("Tablica {}", tab.data);
   return 0;
 }
